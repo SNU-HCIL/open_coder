13f0d07022f566998682d322a4816dce9a449b61
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-06-02 01:11:10 +0900

    multiple file loading synchronously implemented

--- app/core/csv-deserializer-unit.ts
+++ app/core/csv-deserializer-unit.ts
@@ -2,5 +2,40 @@ import { Document } from './document';
 import { Quote } from './quote';
 
 export class CsvDeserializerUnit{
+    private files:Array<File>
+    private loaded:Array<any[]>
     
+    constructor(files:Array<File>)
+    {
+        this.files = files;
+        this.loaded = [];
+    }
+    
+    private combine(): Array<any>{
+        let result = [];
+        
+        for(var element of this.loaded)
+        {
+            result = result.concat(element);
+        }
+        
+        return result;
+    }
+    
+    loadAsync(callback : (data: Array<any>)=>any ) : void{
+        for(var file of this.files)
+        {
+            Papa.parse(file, {
+                header:true, 
+                complete: (result) => {
+                    this.loaded.push(result.data);
+                    if(this.files.length == this.loaded.length)
+                    {
+                        console.log("Loading complete.");
+                        callback(this.combine());
+                    }
+                }
+            });
+        }
+    }
 }
--- app/core/csv-deserializer.service.ts
+++ app/core/csv-deserializer.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Document } from './document';
 import { Quote } from './quote';
+import { CsvDeserializerUnit } from './csv-deserializer-unit';
 
 @Injectable()
 export class CsvDeserializerService{
     convertCsvFiles(files: Array<File>){
         var document: Document = new Document();
-         
-        for(var file of files)
-        {
-            var result = Papa.parse(file, {header:true});
-            console.log(result);
-            //document.quotes.push.apply(document.quotes, result.data.map(function(d){return new Quote(d)}));
-           
-        }
         
-        console.log(`${document.quotes.length} quotes were imported.`);
-        console.log(document);
+        let unit = new CsvDeserializerUnit(files);
+        unit.loadAsync((data:Array<any>)=>{
+            document.quotes = document.quotes.concat(data.map(function(row){return new Quote(row);}));
+            
+            console.log(`${document.quotes.length} quotes were imported.`);
+            console.log(document);
+        });
     }
 }
