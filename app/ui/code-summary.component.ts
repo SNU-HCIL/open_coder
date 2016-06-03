f5560127346c516cd68d207af2d2e76dce122e82
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-06-03 22:04:44 +0900

    fine-tuned logo

--- app/ui/code-summary.component.ts
+++ app/ui/code-summary.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
+import {TitleComponent} from './common/title.component';
 import { Document } from '../core/document';
+
 
 @Component({
   selector: 'oc-code-summary',
   styleUrls: ['app/ui/code-summary.css'],
   templateUrl: 'app/ui/code-summary.html',
-  directives: []
+  directives: [TitleComponent]
 })
 export class CodeSummaryComponent {
     @Input() doc : Document;
