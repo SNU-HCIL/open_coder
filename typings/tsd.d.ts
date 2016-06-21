cbcbbcf1c37e3fa6572cf0634521679d657130f4
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-06-21 22:20:34 +0900

    gitignore update

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="qtip2/qtip2.d.ts" />
+/// <reference path="dropzone/dropzone.d.ts" />
