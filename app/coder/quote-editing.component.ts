432c580d0a03fd22be1d5599b5bfc9fe806bc722
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-06-24 14:19:45 +0900

    new code panel 버그 수정

--- app/coder/quote-editing.component.ts
+++ app/coder/quote-editing.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, Input, Output, OnInit, AfterViewInit, ElementRef, EventEmitter } from '@angular/core';
 import {Quote} from '../core/quote';
 import {Entry} from '../core/entry';
 
@@ -6,6 +6,144 @@ import {ResizableTableColumn} from '../ui/common/resizable-table-column';
 
 import {QuoteEditingState} from './quote-editing-state';
 import {VisualizationInformationService} from '../services/visualization-information.service';
+
+
+
+@Component({
+    selector: 'oc-new-code-panel',
+    styleUrls:[
+        'app/coder/quote-editing.styles.css'
+    ],
+    styles:[
+        `
+        .code{
+            background:#afafaf;
+        }
+
+        .adding_panel{
+        position:relative;
+        float:left;
+        clear:both;
+        }
+
+        .autocomplete_list{
+        position:absolute;
+        background: #fafafa;
+        border: 1px solid #afafaf;
+        border-top: hidden;
+        -webkit-box-shadow: 0px 2px 2px 1px rgba(0,0,0,0.23);
+        -moz-box-shadow: 0px 2px 2px 1px rgba(0,0,0,0.23);
+        box-shadow: 0px 2px 2px 1px rgba(0,0,0,0.23);
+        }
+
+        .autocomplete_list ul{
+        padding: 0px;
+        position: relative;
+        list-style-type: none;
+        }
+
+        .autocomplete_list li{
+        cursor: pointer;
+        width: 100%;
+        margin: 0;
+        }
+
+        .autocomplete_list li div{
+        padding: 6px;
+        }
+
+        .autocomplete_list li:last-child{
+        margin-bottom: 0px;
+        }
+
+        .autocomplete_list li:hover{
+        background: #525252;
+        color: white;
+        }
+        `
+    ],
+    template:
+    `
+        <div class="code adding_panel">
+            <input class="code_name_input" type="text" (input)="newCodeName = $event.target.value" (keyup)="newCodeNameKeyUpEvent($event)">
+            <div *ngIf="autoCompleteList" class="autocomplete_list">
+            <ul>
+                <li *ngFor="let candidate of autoCompleteList" (click)="onCandidateClicked(candidate)">
+                    <div>
+                        {{candidate}}
+                    </div>
+                </li>
+            </ul>
+            </div>
+            <button (click)="onApplyNewCodeButtonClicked()">Add</button>
+            <button (click)="onCancelClicked()" >Cancel</button>
+            <br>
+            <span *ngIf="error" class="error">{{error}}</span>
+        </div>
+    `
+})
+class NewCodePanelComponent implements AfterViewInit{
+    error: string;
+    autoCompleteList : Array<string>;
+    private newCodeName : string;
+    private input$ : JQuery;
+
+    @Output() onNewCodeApply = new EventEmitter();
+    @Output() onCanceled = new EventEmitter();
+
+    @Input() quote : Quote;
+    
+
+    constructor(private elmRef : ElementRef)
+    {
+
+    }
+
+    ngAfterViewInit(){
+        this.input$ = jQuery(this.elmRef.nativeElement).find(".code_name_input") 
+        this.input$.val(null);
+       
+    }
+
+    newCodeNameKeyUpEvent(event : any) : void
+    {
+        let fuse = new Fuse(this.quote.parent.codeCounts.map((cc)=>{return cc.code}), null);
+        console.log(this.newCodeName)
+        let fuzzyIndices = fuse.search(this.newCodeName);
+        if(fuzzyIndices != null && fuzzyIndices.length > 0)
+        {
+            this.autoCompleteList = fuzzyIndices.map((i)=>{return this.quote.parent.codeCounts[i].code});
+        }
+        else{
+            this.autoCompleteList = null
+        }
+    }
+    
+    onCandidateClicked(candidate: string){
+        this.newCodeName = candidate;
+        console.log(this.input$);
+        this.input$.val(this.newCodeName);
+        this.autoCompleteList = null;
+    }
+    
+    onApplyNewCodeButtonClicked()
+    {
+        if(this.quote.codes.map((c)=>{return c.content}).indexOf(this.newCodeName) < 0)
+        {
+            this.error = null
+            this.onNewCodeApply.next(this.newCodeName);
+        }
+        else{
+            this.error = "Duplicate code.";
+        }
+    }
+    
+    onCancelClicked() : void {
+        this.onCanceled.next(this.quote);
+    }
+}
+
+
 
 @Component({
   selector: 'tr.quote_element',
@@ -29,99 +167,45 @@ import {VisualizationInformationService} from '../services/visualization-informa
                 </span>
             </li> 
             <li>
-                <div *ngIf="state == 'adding'" class="code adding_panel">
-                   <input class="code_name_input" type="text" (input)="newCodeName = $event.target.value" (keyup)="newCodeNameKeyUpEvent($event)">
-                   <div *ngIf="autoCompleteList" class="autocomplete_list">
-                    <ul>
-                        <li *ngFor="let candidate of autoCompleteList" (click)="onCandidateClicked(candidate)">
-                            <div>
-                                {{candidate}}
-                            </div>
-                        </li>
-                    </ul>
-                   </div>
-                   <button (click)="onApplyNewCodeButtonClicked()">Add</button>
-                   <button (click)="onCanceled()" >Cancel</button>
-                   <br>
-                   <span *ngIf="error" class="error">{{error}}</span>
-                </div>
-                <button class="circle" *ngIf="state == 'idle'" (click)="onAddCodeButtonClicked()">+</button>
+                <oc-new-code-panel *ngIf="state == 'adding'" [quote]="quote" (onNewCodeApply)="onNewCodeApplied($event)" (onCanceled)="onCanceled($event)"></oc-new-code-panel>
+                <button class="circle add_code" *ngIf="state == 'idle'" (click)="onAddCodeButtonClicked()">+</button>
             </li>
         </ul>
       </td>
     `,
-  directives: []
+  directives: [NewCodePanelComponent]
 })
-export class QuoteEditingComponent implements OnInit, AfterViewInit {
+export class QuoteEditingComponent implements AfterViewInit {
     @Input() quote: Quote
     
-    state: string = "idle"
-    error: string
-    autoCompleteList : Array<string>
-    
-    private newCodeName : string
+    state: string = "idle"   
     
     private resizableTableColumn : ResizableTableColumn;
-    
-    private input$ : JQuery;
-    
+        
     constructor(private elmRef : ElementRef, private visualizationInformationService: VisualizationInformationService)
     {
         
     }
     
-    ngOnInit():any{
-        
-    }
-    
     ngAfterViewInit(){
-        this.resizableTableColumn = new ResizableTableColumn(this.elmRef.nativeElement, "td.handle", "td.content");
-        this.input$ = jQuery(this.elmRef.nativeElement).find(".code_name_input") 
-        this.input$.val(null);
-        
+        this.resizableTableColumn = new ResizableTableColumn(this.elmRef.nativeElement, "td.handle", "td.content"); 
     }
     
     onAddCodeButtonClicked(): void
     {
-        this.error = null
         this.state = "adding"
     }
-    
-    onCanceled() : void {
+
+    onCanceled():void
+    {
         this.state = "idle"
     }
     
-    newCodeNameKeyUpEvent(event : any) : void
+    onNewCodeApplied(newCodeName: string)
     {
-        let fuse = new Fuse(this.quote.parent.codeCounts.map((cc)=>{return cc.code}), null);
-        console.log(this.newCodeName)
-        let fuzzyIndices = fuse.search(this.newCodeName);
-        if(fuzzyIndices != null && fuzzyIndices.length > 0)
-        {
-            this.autoCompleteList = fuzzyIndices.map((i)=>{return this.quote.parent.codeCounts[i].code});
-        }
-        else{
-            this.autoCompleteList = null
-        }
-    }
-    
-    onCandidateClicked(candidate: string){
-        this.newCodeName = candidate;
-        this.autoCompleteList = null;
-    }
-    
-    onApplyNewCodeButtonClicked()
-    {
-        if(this.quote.codes.map((c)=>{return c.content}).indexOf(this.newCodeName) < 0)
-        {
-            this.quote.codes.push(new Entry(this.newCodeName));
-            this.quote.parent.update();
-            this.state = "idle";
-            this.error = null
-        }
-        else{
-            this.error = "Duplicate code.";
-        }
+        this.quote.codes.push(new Entry(newCodeName));
+        this.quote.parent.update();
+        this.state = "idle";
     }
     
     onRemoveCodeClicked(code : Entry){
